0425bc43b17587e67199198cd1040e2c95566239
Adolfo <adolfo.rodriguez2231@gmail.com>
2026-08-25 12:53:02 -0400

    Add AuthenticationError handler and return exception messages in responses

--- backend/app/api/exception_handler.py
+++ backend/app/api/exception_handler.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 
-from app.core.exceptions import UserAlreadyExistsError
+from app.core.exceptions import AuthenticationError, UserAlreadyExistsError
 
 
 async def user_already_exists_handler(
@@ -13,13 +13,30 @@ async def user_already_exists_handler(
     """Return a 409 response when registration email already exists."""
     return JSONResponse(
         status_code=status.HTTP_409_CONFLICT,
-        content={"detail": "Email already registered"},
+        content=exc.message,
+    )
+
+
+async def authentication_error_handler(
+    request: Request,
+    exc: AuthenticationError,
+) -> JSONResponse:
+    """Return a 401 response when user is unautorized"""
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content=exc.message,
     )
 
 
 def register_exception_handlers(app: FastAPI) -> None:
     """Register global exception handlers on the FastAPI app."""
+
     app.add_exception_handler(
         UserAlreadyExistsError,
         user_already_exists_handler,
+    )
+
+    app.add_exception_handler(
+        AuthenticationError,
+        authentication_error_handler,
     )
